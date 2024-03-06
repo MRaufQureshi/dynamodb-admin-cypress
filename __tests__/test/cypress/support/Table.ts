90f4a51351c1b0ae0c8944726ea29d07ee867aff
mraufqureshiwebsite <mraufqureshiwebsite@gmail.com>
2024-03-06 22:53:41 +0100

    02 Test cases for Table details

--- __tests__/test/cypress/support/Table.ts
+++ __tests__/test/cypress/support/Table.ts
@@ -1,14 +1,29 @@
+import {Base} from "../objects/Base";
+import Errors from "undici-types/errors";
+import BalancedPoolMissingUpstreamError = Errors.BalancedPoolMissingUpstreamError;
+
 let tabNames: any
 export class Table {
-    public static assertTabTitles() {
+    public static assertTabTitlesAndURL() {
         const tmpTabs = '.nav-tabs'
+        cy.get(tmpTabs).click()
+        cy.url().should('not.contains', '/items')
         tabNames = [
-            'Items',
             'Get',
             'Meta'
         ]
         tabNames.forEach(function (value) {
             cy.get(tmpTabs).contains(value).click()
+            cy.url().should('include', `/${value.toLowerCase()}`)
         })
+
+        cy.get(tmpTabs).find(Base.tableDetails.TabMeta).click().then(()=>{
+            cy.get(Base.tableDetails.MetaDocumentArea).should('be.visible')
+        })
+
+        cy.get(tmpTabs).find(Base.tableDetails.TabGet).click().then(()=>{
+            cy.get(Base.tableDetails.GetForm).should('be.visible')
+        })
+
     }
 }
