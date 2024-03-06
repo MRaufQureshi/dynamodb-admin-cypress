b4c1e671ecd2e97be0f423f72f6742a4c6491e85
mraufqureshiwebsite <mraufqureshiwebsite@gmail.com>
2024-03-06 23:17:27 +0100

    03 Delete table function

--- __tests__/test/cypress/support/Table.ts
+++ __tests__/test/cypress/support/Table.ts
@@ -1,8 +1,10 @@
 import {Base} from "../objects/Base";
 import Errors from "undici-types/errors";
 import BalancedPoolMissingUpstreamError = Errors.BalancedPoolMissingUpstreamError;
+import {Fixtures} from "../fixtures";
 
 let tabNames: any
+let inputValue = `${Math.random()}`
 export class Table {
     public static assertTabTitlesAndURL() {
         const tmpTabs = '.nav-tabs'
@@ -24,6 +26,20 @@ export class Table {
         cy.get(tmpTabs).find(Base.tableDetails.TabGet).click().then(()=>{
             cy.get(Base.tableDetails.GetForm).should('be.visible')
         })
-
+    }
+    public static addTable(){
+        Fixtures.open('/create-table')
+        cy.get(Base.createTable.InputFieldTableName).type(inputValue)
+        cy.get(Base.createTable.InputFieldHashAttributeName).type(inputValue)
+        cy.get(Base.createTable.DropdownAttributeType).select('String')
+        cy.get(Base.createTable.ButtonSubmit).click()
+        cy.wait(2000)
+        cy.get(Base.landingPage.ListTable).then((tableList)=>{
+            expect(tableList).to.contain(inputValue)
+        })
+    }
+    public static deleteTable(){
+        cy.get(`[data-table-name="${inputValue}"]`)
+            .find('.badge-danger').click({multiple:true})
     }
 }
